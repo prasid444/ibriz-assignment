7cb0614f10e775ae719eb439e968bc736b93696f
prasid444 <prasidkarki95@gmail.com>
2024-02-11 12:18:09 +0545

    fix: update layout of home page

--- src/features/home/pages/HomePage.tsx
+++ src/features/home/pages/HomePage.tsx
@@ -1,11 +1,19 @@
+import { Button } from 'antd';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 export const HomePage = () => {
   return (
-    <div>
-      This is Home Page
-      <Link to="/contract">Go to Contract Page</Link>
+    <div className="h-screen flex flex-row items-center justify-center">
+      <div className="flex flex-col gap-4 justify-center items-center">
+        <h1 className="text-primary font-bold text-2xl">Welcome to Minting Assignment</h1>
+        <div className="text-gray-600 text-xs">Press Continue to Start</div>
+        <Link to={'/contract'}>
+          <Button size="large" type="primary" className="bg-primary">
+            Continue
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 };
