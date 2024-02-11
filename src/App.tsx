fface20ee5d2262f0468ac2ace6a09c8d31b1945
prasid444 <prasidkarki95@gmail.com>
2024-02-11 09:29:34 +0545

    feat: add routing with 2 features

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { ContractPage } from 'features/contract/pages/ContractPage';
+import { HomePage } from 'features/home/pages/HomePage';
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import './App.css';
 
 function App() {
-  return <div>Hi</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={HomePage} />
+        <Route path="/contract" Component={ContractPage} />
+        {/* TODO: To add redirection or no page found */}
+        <Route path="*" Component={HomePage} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/features/contract/pages/ContractPage.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const ContractPage = () => {
+  return <div>This is Contract Page</div>;
+};
